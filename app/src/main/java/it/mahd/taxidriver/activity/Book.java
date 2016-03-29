8a18de3ed879ec8dc96857b78711af75bc1ff5f3
Salem <salemmosbahi@gmail.com>
2016-03-29 15:24:17 +0100

    direction marker

--- app/src/main/java/it/mahd/taxidriver/activity/Book.java
+++ app/src/main/java/it/mahd/taxidriver/activity/Book.java
@@ -2,10 +2,13 @@ package it.mahd.taxidriver.activity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,25 +21,38 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.IBinder;
 import android.provider.Settings;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.Socket;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import it.mahd.taxidriver.Main;
 import it.mahd.taxidriver.R;
 import it.mahd.taxidriver.util.Controllers;
+import it.mahd.taxidriver.util.DirectionMap;
 import it.mahd.taxidriver.util.SocketIO;
 
 /**
@@ -50,6 +66,7 @@ public class Book extends Fragment implements LocationListener {
     MapView mMapView;
     Service service;
     private GoogleMap googleMap;
+    ArrayList<LatLng> markerPoints;
     protected LocationManager locationManager;// Declaring a Location Manager
     Location location; // location
     private CameraPosition cameraPosition;
@@ -62,6 +79,7 @@ public class Book extends Fragment implements LocationListener {
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 3;// The minimum distance to change Updates in meters // 3 meters
     private static final long MIN_TIME_BW_UPDATES = 1000 * 3 * 1;// The minimum time between updates in milliseconds // 3 seconds
     private FloatingActionButton Start_btn, Pause_btn;
+    private TextView DistanceDuration_txt;
 
     public Book() {}
 
@@ -77,8 +95,10 @@ public class Book extends Fragment implements LocationListener {
         socket.connect();
 
         mMapView = (MapView) v.findViewById(R.id.mapView);
+        DistanceDuration_txt = (TextView) v.findViewById(R.id.distance_time_txt);
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();
+        markerPoints = new ArrayList<LatLng>();
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
@@ -101,6 +121,40 @@ public class Book extends Fragment implements LocationListener {
         cameraPosition = new CameraPosition.Builder().target(new LatLng(latitude, longitude)).zoom(15).build();
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            public void onMapClick(LatLng point) {
+                // Already two locations
+                if(markerPoints.size()>1){
+                    markerPoints.clear();
+                    googleMap.clear();
+                }
+                // Adding new item to the ArrayList
+                markerPoints.add(point);
+                // Creating MarkerOptions & Setting the position of the marker
+                MarkerOptions options = new MarkerOptions();
+                options.position(point);
+                //For the start location, the color of marker is BLUE and for the end location, the color of marker is RED.
+                if(markerPoints.size()==1){
+                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                }else if(markerPoints.size()==2){
+                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                }
+                // Add new marker to the Google Map Android API V2
+                googleMap.addMarker(options);
+                // Checks, whether start and end locations are captured
+                if(markerPoints.size() >= 2){
+                    LatLng origin = markerPoints.get(0);
+                    LatLng dest = markerPoints.get(1);
+                    // Getting URL to the Google Directions API
+                    String url = getDirectionsUrl(origin, dest);
+                    // Start downloading json data from Google Directions API
+                    DownloadTask downloadTask = new DownloadTask();
+                    downloadTask.execute(url);
+                }
+
+            }
+        });
+
         Start_btn = (FloatingActionButton) v.findViewById(R.id.start_btn);
         Start_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,6 +173,132 @@ public class Book extends Fragment implements LocationListener {
             }
         });
         return v;
+    }
+
+    private String getDirectionsUrl(LatLng origin,LatLng dest){
+        // Origin of route
+        String str_origin = "origin="+origin.latitude+","+origin.longitude;
+        // Destination of route
+        String str_dest = "destination="+dest.latitude+","+dest.longitude;
+        // Sensor enabled
+        String sensor = "sensor=false";
+        // Building the parameters to the web service
+        String parameters = str_origin+"&"+str_dest+"&"+sensor;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
+        return url;
+    }
+    /** A method to download json data from url */
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+            // Connecting to url
+            urlConnection.connect();
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+            StringBuffer sb  = new StringBuffer();
+            String line = "";
+            while( ( line = br.readLine())  != null){
+                sb.append(line);
+            }
+            data = sb.toString();
+            br.close();
+        }catch(Exception e){
+        }finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+    // Fetches data from url passed
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+        // Downloading data in non-ui thread
+        @Override
+        protected String doInBackground(String... url) {
+            // For storing data from web service
+            String data = "";
+            try{
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+            }catch(Exception e){
+            }
+            return data;
+        }
+        // Executes in UI thread, after the execution of doInBackground()
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            ParserTask parserTask = new ParserTask();
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+        }
+    }
+    /** A class to parse the Google Places in JSON format */
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+            try{
+                jObject = new JSONObject(jsonData[0]);
+                DirectionMap parser = new DirectionMap();
+                // Starts parsing data
+                routes = parser.parse(jObject);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return routes;
+        }
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions lineOptions = null;
+            String distance = "";
+            String duration = "";
+            if(result.size()<1){
+                Toast.makeText(getActivity(), "No Points", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            // Traversing through all the routes
+            for(int i=0;i<result.size();i++){
+                points = new ArrayList<LatLng>();
+                lineOptions = new PolylineOptions();
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+                // Fetching all the points in i-th route
+                for(int j=0;j<path.size();j++){
+                    HashMap<String,String> point = path.get(j);
+                    if(j==0){	// Get distance from the list
+                        distance = (String)point.get("distance");
+                        continue;
+                    }else if(j==1){ // Get duration from the list
+                        duration = (String)point.get("duration");
+                        continue;
+                    }
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+                    points.add(position);
+                }
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(2);
+                lineOptions.color(Color.RED);
+            }
+            DistanceDuration_txt.setText("Distance:"+distance + ", Duration:"+duration);
+            // Drawing polyline in the Google Map for the i-th route
+            googleMap.addPolyline(lineOptions);
+        }
     }
 
     private void sendToServer(double lat, double lon, boolean work) {
